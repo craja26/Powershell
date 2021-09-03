35aa7b926e4d530c2de6a5feacc804338a5b44ae
Raja <craja26@gmail.com>
2021-09-03 14:34:23 +0200

    Create general_commands.ps1

---
+++ general_commands.ps1
@@ -0,0 +1,2 @@
+#$password = Read-Host -assecurestring "Please enter your password"
+#$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
