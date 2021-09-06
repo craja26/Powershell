9a079663c56d18cee92f042699abe62aa4017770
Raja <craja26@gmail.com>
2021-09-06 16:59:25 +0200

    Update clone_db_with_new_version.ps1

--- clone_db_with_new_version.ps1
+++ clone_db_with_new_version.ps1
@@ -1,22 +1,9 @@
 #function for checking connection
 function check-connection($connection_str_db1){
-    write-host "Connection String: " $connection_str_db1
     TRY{
 		$Connection = New-Object System.Data.SQLClient.SQLConnection
 		$Connection.ConnectionString = $connection_str_db1#"Server= $srv;Integrated Security=false;Initial Catalog=master;uid=$username; pwd=$password;"
 		$Connection.Open()
-		#$Command = New-Object System.Data.SQLClient.SQLCommand
-		#$Command.Connection = $Connection
-		# update sql query as per your requirements
-		#$Command.CommandText = $sqlcmd
-		#$Reader = $Command.ExecuteReader()
-		#$intCounter = 0
-        #$report_out = @()
-        #$Datatable = New-Object System.Data.DataTable
-        #$Datatable.Load($Reader)
-		#$report_out
-        #$result_set
-        #return $Datatable
 		$Connection.Close()
         return $true
 	}CATCH{
@@ -25,7 +12,6 @@ function check-connection($connection_str_db1){
 	}
 }
 function get-sql-query-results($connection_str, $sqlcmd){
-    $srv = "localhost"
     TRY{
 		$Connection = New-Object System.Data.SQLClient.SQLConnection
 		$Connection.ConnectionString = $connection_str
@@ -36,23 +22,8 @@ function get-sql-query-results($connection_str, $sqlcmd){
 		$Command.CommandText = $sqlcmd
 		$Reader = $Command.ExecuteReader()
 		$intCounter = 0
-        $report_out = @()
         $Datatable = New-Object System.Data.DataTable
         $Datatable.Load($Reader)
-		# while($Reader.Read())
-		#  {
-		# 	$name = $Reader["name"].ToString()    
-		# 	$database_id = $Reader["database_id"].ToString()
-		# 	#Write-Host "$SERVERNAME $PRODUCTLEVEL $intCounter"
-        #     $report = New-Object -TypeName PSObject
-        #     $report | Add-Member -MemberType NoteProperty -Name 'Name' -Value $name
-        #     $report | Add-Member -MemberType NoteProperty -Name 'database_id' -Value $database_id
-		# 	$report_out += $report
-		# 	$intCounter++
-		#  }  
-		 $report_out
-         #$result_set
-         
 		$Connection.Close()
         return $Datatable
 	}CATCH{
@@ -65,10 +36,14 @@ $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Ru
 $server_db1 = "localhost"
 $server_db2 = "."
 $database = "SQLLogging"
-#write-host "password is: " $password
+$web_db_str = "STAGE_Web" 
 $new_db_name = ""
 $current_db_name = ""
 $last_full_backup
+$backup_old_days
+$backup_file
+$required_backup_type
+$backup_command
 $result_set = @()
 $result_db_info = @()
 # Check server db1 connection
@@ -92,41 +67,75 @@ if ($connection_status -eq "True"){
     exit
 }
 # Get latest DB backup information
-$sqlcmd_db_info = "SELECT  name , 
+$sqlcmd_db_info = "DECLARE @row_cnt int
+IF OBJECT_ID('tempdb..#temp_db_backup_info') IS NOT NULL DROP TABLE #temp_db_backup_info
+IF OBJECT_ID('tempdb..#temp_physical_file') IS NOT NULL DROP TABLE #temp_physical_file
+CREATE TABLE #temp_db_backup_info(slno int identity(1,1)
+	, name varchar(100)
+	, new_db_version int
+	, last_full_backup datetime
+	, last_differential_backup datetime
+	, last_log_backup datetime
+	, last_full_backup_physical_file varchar(2000)
+	, backup_old int)
+CREATE TABLE #temp_physical_file(slno int identity(1,1), database_name varchar(100), physical_device_name varchar(500),backup_finish_date datetime)
+INSERT INTO #temp_db_backup_info(name, new_db_version, last_full_backup, last_differential_backup, last_log_backup)
+SELECT  name , 
 	SUBSTRING(name, PATINDEX('%[0-9]%', name), PATINDEX('%[0-9][^0-9]%', name + 't') - PATINDEX('%[0-9]%', name) + 1) + 5 AS new_db_version,
-    CASE WHEN PATINDEX('%[a-z]%', REVERSE(name)) > 1
-        THEN LEFT(name, LEN(name) - PATINDEX('%[a-z]%', REVERSE(name)) + 1)
-    ELSE '' END AS db_string_part,
-    recovery_model_desc ,
-    state_desc ,
     d AS 'last_full_backup' ,
-    i AS 'Last Differential Backup' ,
-    l AS 'Last log Backup'--, physical_device_name
+    i AS 'last_differential_backup' ,
+    l AS 'last_log_backup'
 FROM (SELECT db.database_id ,db.name ,
             db.state_desc ,
             db.recovery_model_desc ,
-            type ,
-            backup_finish_date--, bmf.physical_device_name
+			type ,
+            backup_finish_date
 	FROM  master.sys.databases db
         LEFT OUTER JOIN msdb.dbo.backupset a ON a.database_name = db.name 
-		--LEFT OUTER JOIN msdb.dbo.backupmediafamily bmf ON a.media_set_id = bmf.media_set_id
-		where database_id > 4 and db.is_distributor <> 1 AND db.name like 'STG_Web%' 
+		where database_id > 4 and db.is_distributor <> 1 AND db.name like '$web_db_str'--'BSG-STAGE_Web%' 
     ) AS Sourcetable 
 PIVOT 
-    ( MAX(backup_finish_date) FOR type IN ( D, I, L ) ) AS MostRecentBackup"
+    ( MAX(backup_finish_date) FOR type IN ( D, I, L ) ) AS MostRecentBackup
+SET @row_cnt = @@ROWCOUNT
+IF (@row_cnt = 1)
+BEGIN
+	INSERT INTO #temp_physical_file(database_name, physical_device_name, backup_finish_date)
+	SELECT TOP 1
+		bs.database_name,
+		bmf.physical_device_name,
+		bs.backup_finish_date
+	FROM   msdb.dbo.backupmediafamily AS bmf
+	INNER JOIN msdb.dbo.backupset AS bs ON bmf.media_set_id = bs.media_set_id 
+		WHERE bs.database_name in (select name from #temp_db_backup_info) AND (CONVERT(datetime, bs.backup_start_date, 102) >= GETDATE() - 17)
+		AND bs.type = 'D' 
+	ORDER BY 
+		bs.backup_start_date desc
+END
+UPDATE tbi SET backup_old = DATEDIFF(day, tbi.last_full_backup, GETDATE()), tbi.last_full_backup_physical_file = tpf.physical_device_name
+FROM  #temp_db_backup_info tbi INNER JOIN #temp_physical_file tpf ON tbi.name = tpf.database_name WHERE tbi.last_full_backup = tpf.backup_finish_date
+SELECT slno, name, new_db_version, last_full_backup, last_differential_backup, last_full_backup_physical_file
+FROM #temp_db_backup_info
+IF OBJECT_ID('tempdb..#temp_db_backup_info') IS NOT NULL DROP TABLE #temp_db_backup_info
+IF OBJECT_ID('tempdb..#temp_physical_file') IS NOT NULL DROP TABLE #temp_physical_file"
 
 [array]$result_db_info = @(get-sql-query-results $connection_str_db1 $sqlcmd_db_info)
-write-host "Second-SQL Result: " $result_db_info.name.Count
+write-host "Second-SQL Result count: " $result_db_info.name.Count
 if ($result_db_info.name.Count -eq 1){
     ForEach($row_db_info in $result_db_info){    
-        #write-host "row $i Database name: " $row_db_info.name
-        #write-host "row $i new_db_version: " $row_db_info.new_db_version
-        #write-host "row $i Last_full_backup: " $row_db_info.last_full_backup    
-        #write-host "row $i db_string_part: " $row_db_info.db_string_part 
-        $new_db_name = $row_db_info.db_string_part + $row_db_info.new_db_version
-        $current_db_name = $row_db_info.name
-        $last_full_backup = $row_db_info.last_full_backup
+        $new_db_name        = $web_db + $row_db_info.new_db_version
+        $current_db_name    = $row_db_info.name
+        $last_full_backup   = $row_db_info.last_full_backup
+        $backup_old_days    = $row_db_info.backup_old
+        $backup_file        = $row_db_info.last_full_backup_physical_file
         $i++
+    }
+    write-host "last full backup: " $backup_file
+    if(($backup_file.length -lt 8) -or ($backup_old_days -gt 3)){
+        write-host "No recent full backup is available. Creating new DB backup."
+        $required_backup_type = "FULL"
+        $backup_command = "BACKUP DATABASE [$current_db_name] TO DISK = ''"
+    }else{
+        $required_backup_type = "DIFF"
     }
 }elseif($result_db_info.name.Count -gt 1){
     write-host "There are two web db exists. Might forgot to drop old database or something went wrong. Please check..."
@@ -135,7 +144,8 @@ if ($result_db_info.name.Count -eq 1){
     write-host "There is no web db exists. Please check..."
     exit
 }
-$i = 1
-    
+
+
 write-host "current web database name: " $current_db_name
 write-host "New web database name: " $new_db_name
+write-host "Require backup type: " $required_backup_type
